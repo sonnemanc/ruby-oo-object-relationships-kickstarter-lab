786b947c83b09381fc8ce74506d4cbe561cf38d6
Corban <sonnemanc@gmail.com>
2021-09-14 12:30:43 -0600

    commit tree 71b43829725ccdf095ec1368bb825504bdc3b3b6

---
+++ lib/project_backer.rb
@@ -0,0 +1,21 @@
+class ProjectBacker
+
+    attr_reader :project,   :backer
+
+    @@all = []
+
+    def initialize(project, backer)
+        @project = project
+        @backer = backer
+        save
+    end
+
+    def save
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+end
